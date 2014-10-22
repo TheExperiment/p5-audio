623b06599b5c1e7dd84bdd8904b68334d2da94d5
Nick Velloff <nick.velloff@gmail.com>
2014-10-22 00:28:02 -0400

    work

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -17,9 +17,16 @@ function draw() {
 //    for (i = 0; i<spectrum.length; i++) {
 //     vertex(i, map(spectrum[i], 0, 255, height, 0) );
 //    }
-	
-   var energy = fft.getEnergy(248);
-   ellipse(width/2, constrain(height-energy*height*5, 0, height), 10, 10)
+	var energy = [];
+   energy[0] = fft.getEnergy(90);
+   energy[1] = fft.getEnergy(123.47);
+   energy[2] = fft.getEnergy(146.83);
+   energy[3] = fft.getEnergy(196.00);
+   energy[4] = fft.getEnergy(392.00);
+   
+//    ellipse(width/2, constrain(height-energy*height, 0, height), 10, 10)
    console.log(energy)
-   endShape();
+//    endShape();
 }
+
+//G2 90hz / B2 123.47hz / D3 146.83hz / G3 196.00 hz / B3 246.94hz / G4 392.00
