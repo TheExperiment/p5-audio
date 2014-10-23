e904356978bf05d28bba40c2b21aac875f9247ec
Nick Velloff <nick.velloff@gmail.com>
2014-10-23 17:32:04 -0400

    work

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,17 +1,18 @@
 var mic, fft, didAnalyze;
 var filter, filterFreq, filterWidth;
 
+
 function setup() {
    createCanvas(710,400);
+   
+   background(200);
+  	
    mic = new p5.AudioIn();
    mic.start();
+    
+   
    fft = new p5.FFT();
    fft.setInput(mic);
-   background(200);
-   
-   filter = new p5.BandPass();
-   filter.process(mic);
-   
 }
 
 function draw() {
@@ -21,11 +22,6 @@ function draw() {
    var spectrum = fft.analyze();
 	
 // 	if ( didRiseAbove(volume) && !didAnalyze ) {
-
-		
-		var freq = map (80, 150);
-  		filter.freq(freq);
-  		filter.res(50);
   		
 		background(200);
 		didAnalyze = true;
@@ -34,8 +30,6 @@ function draw() {
 			vertex(i, map(spectrum[i], 0, 255, height, 0) );
 		}
 		endShape();
-		
-		console.log(spectrum.length);
 // 	}
    
 }
@@ -44,49 +38,3 @@ function didRiseAbove(volume) {
 	var threshold = 0.1;
   	return (volume > threshold)
 }
-
-/*
-
-// Adapted from Learning Processing, Daniel Shiffman
-// learningprocessing.com
-var input;
-var analyzer;
-
-function setup() {
-  createCanvas(710, 200);
-  background(255);
-
-  // Create an Audio input
-  input = new p5.AudioIn();
-
-  input.start();
-}
-
-function draw() {
-  // Get the overall volume (between 0 and 1.0)
-  var volume = input.getLevel();
-
-  // If the volume > 0.1,  a rect is drawn at a random location. 
-  // The louder the volume, the larger the rectangle.
-  var threshold = 0.1;
-  if (volume > threshold) {
-    stroke(0);
-    fill(0, 100);
-    rect(random(40, width), random(height), volume*50, volume*50);
-  }
-
-  // Graph the overall potential volume, w/ a line at the threshold
-  var y = map(volume, 0, 1, height, 0);
-  var ythreshold = map(threshold, 0, 1, height, 0);
-
-  noStroke();
-  fill(175);
-  rect(0, 0, 20, height);
-  // Then draw a rectangle on the graph, sized according to volume
-  fill(0);
-  rect(0, y, 20, y);
-  stroke(0);
-  line(0, ythreshold, 19, ythreshold);
-}
-
-*/
